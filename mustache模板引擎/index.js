9ecaf831dfc84f8d784fcbd6a88918f37befd7ad
xujintai-jt <1791639435@qq.com>
2021-05-10 19:48:44 +0800

    tokens生成器简化

--- mustache模板引擎/index.js
+++ mustache模板引擎/index.js
@@ -6,28 +6,21 @@ window.TemplateEngine = {
     let end = "}}";
     let scanner = new Scanner(template);
 
-
     while (scanner.tail !== "") {
       //若tail中不存在"{{"和"}}"，则scanUtil()
-      if (scanner.tail.indexOf(start) === -1 && scanner.tail.indexOf(end) === -1) {
+      const flag = scanner.tail.indexOf(start) < 0 && scanner.tail.indexOf(end) < 0;
+      if (flag) {
         const text = scanner.scanUtil();
         arr.push(["text", text]);
-        continue
+        continue;
       }
-        //若tail中"{{"的index小于"}}"的index,则scanUtil("{{")、scan("{{");(tail中"{{"不为-1)
-        if (
-          scanner.tail.indexOf(start) < scanner.tail.indexOf(end) &&
-          scanner.tail.indexOf(start) !== -1
-        ) {
-          const text = scanner.scanUtil(start);
-          arr.push(["text", text]);
-          scanner.scan(start);
-          continue
-        } 
-          //若tail中"}}"的index小于"{{"的index,则scanUtil("}}")、scan("}}")
-          const name = scanner.scanUtil(end);
-          arr.push(["name", name]);
-          scanner.scan(end);
+     //存在"{{"和"}}"，则scan一套"{{"和"}}"
+      const text = scanner.scanUtil(start);
+      arr.push(["text", text]);
+      scanner.scan(start);
+      const name = scanner.scanUtil(end);
+      arr.push(["name", name]);
+      scanner.scan(end);
     }
 
     console.log(arr);
