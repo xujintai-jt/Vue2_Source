5ee7d701862dbc9d365e4d410456bc9dc3497005
xujintai-jt <1791639435@qq.com>
2021-05-10 20:34:20 +0800

    处理循环体

--- mustache模板引擎/index.js
+++ mustache模板引擎/index.js
@@ -1,27 +1,9 @@
-import Scanner from "./Scaner";
+import parseTemplateToToken from "./parseTemplateToToken";
+
+
 window.TemplateEngine = {
   render(template, data) {
-    let arr = [];
-    let start = "{{";
-    let end = "}}";
-    let scanner = new Scanner(template);
-
-    while (scanner.pos !== scanner.templateStr.length) {
-      //存在"{{"和"}}"，则scan一套"{{"和"}}"
-      const text = scanner.scanUtil(start);
-      //如果不是空字符串，则添加
-      if (text) {
-        arr.push(["text", text]);
-        scanner.scan(start);
-      }
-
-      const name = scanner.scanUtil(end);
-      if (name) {
-        arr.push(["name", name]);
-        scanner.scan(end);
-      }
-    }
-
-    console.log(arr);
-  },
-};
+    let tokens = parseTemplateToToken(template)
+    console.log(tokens);
+  }
+}
---
+++ mustache模板引擎/parseTemplateToToken.js
@@ -0,0 +1,33 @@
+import Scanner from "./Scaner";
+
+export default function parseTemplateToToken(template) {
+  let tokens = [];
+  //创建收集器
+  let scanner = new Scanner(template);
+
+  while (scanner.pos !== scanner.templateStr.length) {
+    //存在"{{"和"}}"，则scan一套"{{"和"}}"
+    const text = scanner.scanUtil("{{");
+    //如果不是空字符串，则添加
+    if (text) {
+      tokens.push(["text", text]);
+    }
+    //跨过"{{"大括号
+    scanner.scan("{{");
+
+    const name = scanner.scanUtil("}}");
+    if (name) {
+      if (name[0] === "#") {
+        tokens.push(["#", name.substring(1)]);
+      } else if (name[0] === "/") {
+        tokens.push(["/", name.substring(1)]);
+      } else {
+        tokens.push(["name", name]);
+      }
+    }
+    //跨过"}}"大括号
+    scanner.scan("}}");
+  }
+
+  return tokens;
+}
