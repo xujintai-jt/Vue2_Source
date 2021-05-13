0851aea35d084c1317bd9bb2bb4a49a4bd423536
xujintai-jt <1791639435@qq.com>
2021-05-13 15:47:12 +0800

    完成{{.}}模板情况bug修复

--- mustache模板引擎/index.js
+++ mustache模板引擎/index.js
@@ -5,8 +5,7 @@ import renderTemplate from "./renderTemplate"
 window.TemplateEngine = {
   render(template, data) {
     let tokens = parseTemplateToToken(template)
-    let resDOM = renderTemplate(tokens,data)
-    console.log(tokens);
-    // console.log(resDOM);
+    let resDOM = renderTemplate(tokens, data)
+    console.log(resDOM);
   }
 }
--- mustache模板引擎/renderTemplate.js
+++ mustache模板引擎/renderTemplate.js
@@ -6,17 +6,21 @@ export default function renderTemplate(tokens, data) {
     if (item[0] === "text") {
       resStr += item[1];
     } else if (item[0] === "name") {
+      //考虑到item[1]可能是"a.b.c"字符串形式，所以使用lookup进行处理
       resStr +=  lookup(data,item[1])
     } else if (item[0] === "#") {
-      //item[1]：数组名，student:[{1},{2}...];
+      //item[1]：数组名，"student".
+      // data[item[1]](lookup(data,item[1])),数组数据，student: [{ 1}, { 2}...];
       //item[2]: 子tokens
-      data[item[1]].forEach((itemObj) => {
+      lookup(data,item[1]).forEach((itemObj) => {
         //itemObj:数组项，student:[{1},{2}...]中的{1},{2}...
-        resStr += renderTemplate(item[2], itemObj);
+        resStr += renderTemplate(item[2], {
+          ".": itemObj,
+          ...itemObj
+        });
       });
     }
   });
 
-  console.log(resStr);
   return resStr;
 }
