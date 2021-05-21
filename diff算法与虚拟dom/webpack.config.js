01b5426d2e1b25dddec8b23f14e5dabad4da7de8
xujintai-jt <1791639435@qq.com>
2021-05-21 13:37:03 +0800

    创建diff算法文件夹，将webpack版本更新为5

--- diff算法与虚拟dom/webpack.config.js
+++ diff算法与虚拟dom/webpack.config.js
@@ -1,11 +1,12 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-  mode:"development",
-  entry: './index.js',
+  mode: "development",
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    //虚拟打包路径
+    publicPath: "/xuni/",
+    filename: "bundle.js",
   },
   //使用后js文件热更新、html文件不会
   devServer: {
@@ -13,7 +14,5 @@ module.exports = {
     contentBase: path.join(__dirname, "dist"),
     compress: true,
     port: 9000,
-    //虚拟打包路径
-    publicPath:'/xuni/'
-  }
+  },
 };
