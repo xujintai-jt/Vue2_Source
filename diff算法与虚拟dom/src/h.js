c67f1bff3d52da9a26b2e414660f13f5734c016f
xujintai-jt <1791639435@qq.com>
2021-05-23 21:41:27 +0800

    完善h函数对数组参数的h()参数判断

--- diff算法与虚拟dom/src/h.js
+++ diff算法与虚拟dom/src/h.js
@@ -2,7 +2,7 @@ import vnode from "./vnode";
 
 //h("div",{},"文字")=>{sel:"div",data:{},children:undefined,text:"文字",elm:undefined}
 //h("div",{},h())=>
-//h("div",{},[])
+//h("div",{},[h(),h()...])
 
 export default function h(sel, data, c) {
   //1.此函数只考虑三个参数的情况
@@ -16,10 +16,26 @@ export default function h(sel, data, c) {
   //2.2 第三个参数为h(),h()的返回参数为对象
   else if (typeof c === "object" && !(c instanceof Array)) {
     return vnode(sel, data, c, undefined, undefined);
-  } else if (c instanceof Array) {
-    // const arr = c.reduce((acc, cur) => acc.push(cur), []);
-    // 使用reduce时大部分情况下不能用push，因为push的返回值是数组长度，而不是数组
-    const arr = c.reduce((acc, cur) => acc.concat(cur), []);
+  }
+  //2.3 第三个参数为数组,[h(),h()...]
+  else if (c instanceof Array) {
+    // 使用reduce时大部分情况下不能用push而是用concat，因为push的返回值是数组长度，而不是数组
+    // const arr = c.reduce((acc, cur) => { acc.concat(cur) }, []);
+    // 增加判断
+    const arr=[]
+    for (var i = 0; i < c.length; i++){
+  
+      if (typeof c[i] === "object" && c[i].hasOwnProperty("sel")) {
+        arr.push(c[i])
+      }
+      else {    console.log(c[i]);
+        throw Error(`传入数组的第${i+1}项参数类型不符合！`);
+      }
+    }
     return vnode(sel, data, arr, undefined, undefined);
+  }
+
+  else {
+    throw Error("传入的第三个参数类型不符合！");
   }
 }
--- diff算法与虚拟dom/src/index.js
+++ diff算法与虚拟dom/src/index.js
@@ -1,8 +1,9 @@
 import h from './h'
-import {vnode} from 'snabbdom'
+// import {vnode} from 'snabbdom'
+// const test = vnode("div",{},[h("div",{},"文字1"),h("div",{},"文字2")])
+// console.log(test);
+
 
 const vnode1 = h("div",{},[h("div",{},"文字1"),h("div",{},"文字2")])
 console.log(vnode1);
 
-const test = vnode("div",{},[h("div",{},"文字1"),h("div",{},"文字2")])
-console.log(test);
